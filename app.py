61a8929ee23ded33b687cae5205357d963662d83
ngnxuanhoa <xuan.n.hoa@gmail.com>
2025-03-16 22:23:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,8 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
 def cleanup():
     print("Đang dừng camera và giải phóng GPIO...")
     picam2.stop()
